2b7e70154f72fe155888a74809aee167a00ff4bb
Ethan-Villanueva <e.villanueva.cs@outlook.com>
2025-11-13 18:07:30 +0000

    Implemented ray class and camera/viewport setup

--- main.cpp
+++ main.cpp
@@ -2,28 +2,52 @@
 #include <fstream>
 #include "vec3.h"
 #include "colour.h"
+#include "ray.h"
+
+const double ASPECT_RATIO = 16.0 / 9.0;
+
+const colour ray_colour(const ray& r) {
+    return colour(0,0,0);
+}
 
 int main() {
     // Image
-    int image_width = 256;
-    int image_height = 256;
+    int image_width = 400;
+    int image_height = std::max(1, static_cast<int>(image_width / ASPECT_RATIO));
+
+    // Camera setup
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
+    point3 camera_point(0,0,0);
+
+    vec3 viewport_u(viewport_width, 0, 0);
+    vec3 viewport_v(0, -viewport_height, 0);
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    auto viewport_upper_left = camera_point - vec3(0,0,focal_length) - (viewport_u / 2) - (viewport_v / 2);
+    auto pixel_centre_top_corner = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
     std::ofstream image_file("image.ppm");
 
     // Render
     image_file << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = 0; j < image_height; j++) {
-        std::cout << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; i++) {
-            colour pixel(static_cast<double>(i) / (image_width-1),
-                         static_cast<double>(j) / (image_height-1),
-                         0.0);
+    for (int i = 0; i < image_height; i++) {
+        std::clog << "\rScanlines remaining: " << (image_height - i) << ' ' << std::flush;
+        for (int j = 0; j < image_width; j++) {
+            auto pixel_centre = pixel_centre_top_corner + (j * pixel_delta_u) - (i * pixel_delta_v); 
+            auto ray_direction = pixel_centre - camera_point;
 
+            ray r(camera_point, ray_direction);
+
+            colour pixel = ray_colour(r);
             write_colour(image_file, pixel);
         }
     }
 
     image_file.close();
 
-    std::cout << "\rDone.                 \n";
+    std::clog << "\rDone.                 \n";
 }
